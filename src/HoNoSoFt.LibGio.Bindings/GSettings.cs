8afeb0a22af4706014c6e473f609e08b975f048a
Nordes Lamarre <nordes.menard-lamarre@konicaminolta.com>
2019-03-12 15:08:39 +0900

    Init

---
+++ src/HoNoSoFt.LibGio.Bindings/GSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HoNoSoFt.LibGio.Bindings
+{
+    public static class GSettings
+    {
+        // https://developer.gnome.org/gio/stable/GSettings.html#g-settings-set-int64
+        // https://developer.gnome.org/glib/stable/glib-Basic-Types.html#gint
+
+        /// <summary>
+        /// Gets the GSettings pointer for the requested schema.
+        /// </summary>
+        /// <param name="schema"></param>
+        /// <returns></returns>
+        [DllImport("libgio-2.0.so", EntryPoint = "g_settings_new")]
+        public static extern IntPtr New(string schema);
+
+        [DllImport("libgio-2.0.so", EntryPoint = "g_settings_get_int")]
+        public static extern int GetInt(IntPtr settings, string key);
+
+        [DllImport("libgio-2.0.so", EntryPoint = "g_settings_set_int")]
+        public static extern bool SetInt(IntPtr settings, string key, int value);
+
+        [DllImport("libgio-2.0.so", EntryPoint = "g_settings_get_int64")]
+        public static extern long GetInt64(IntPtr settings, string key);
+
+        [DllImport("libgio-2.0.so", EntryPoint = "g_settings_set_int64")]
+        public static extern bool SetInt64(IntPtr settings, string key, long value);
+    }
+}
---
+++ src/HoNoSoFt.LibGio.ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HoNoSoFt.LibGio.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Schema to look: ");
+            var gsettingsSchema = Console.ReadLine();
+            Console.Write("Key to look: ");
+            var gsettingsKey = Console.ReadLine();
+
+            var gsettings = HoNoSoFt.LibGio.Bindings.GSettings.New(gsettingsSchema);
+            var result = HoNoSoFt.LibGio.Bindings.GSettings.GetInt(gsettings, gsettingsKey);
+            Console.WriteLine($"Value is: {result}");
+            Console.ReadKey();
+        }
+    }
+}
